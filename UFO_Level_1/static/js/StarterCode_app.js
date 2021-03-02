21aac9bae58c6da3e93064f5b705e251392f5bed
David K High Jr <dhigh@Davids-MBP.hsd1.il.comcast.net>
2021-03-02 07:52:50 -0600

    'Done-ish'

--- UFO_Level_1/static/js/StarterCode_app.js
+++ UFO_Level_1/static/js/StarterCode_app.js
@@ -1,7 +1,5 @@
-// from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
 // Define a function to append table based on reports data
 function appendTable(report) {
     var tbody = d3.select("tbody");
@@ -22,30 +20,33 @@ function appendTable(report) {
         }
     });
 }
-// Display the entire dataset as default
+
+// Display dataset as default
 tableData.forEach(appendTable);
 
 // Select the submit button
 var submit = d3.select("#filter-btn");
 
+// Part 1: Use a date form in your HTML document and write JavaScript code that will listen for events and search through the date/time column to find rows that match user input.
+
 // Click event of datetime filter
 submit.on("click", function() {
 
 // Remove existing table
-   d3.select("tbody").html("");
+d3.select("tbody").html("");
 
 // Prevent the page from refreshing
-   d3.event.preventDefault();
+d3.event.preventDefault();
 
-// Get the value property of the input element
-    var dateTime = d3.select("#datetime").property("value");
-   console.log(dateTime);
+var dateTime = d3.select("#datetime").property("value");
+console.log(dateTime);
 
 // Filter reports
-   var filteredData = tableData.filter(record => record.datetime === dateTime);
-    console.log(filteredData)
+var filteredData = tableData.filter(record => record.datetime === dateTime);
+console.log(filteredData)
 
 // Display the filtered dataset
-    filteredData.forEach(appendTable);
+filteredData.forEach(appendTable);
 
- });
+});
+
